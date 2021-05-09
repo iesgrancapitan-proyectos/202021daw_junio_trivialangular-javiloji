029a58f5d4b0b9504cd052de6c1cf146f9b11c76
javier <javierlopera988@gmail.com>
2021-05-09 18:43:20 +0200

    1

---
+++ config/routes.php
@@ -0,0 +1,20 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\App;
+
+return function (App $app) {
+    $app->get('/hello', function (
+        ServerRequestInterface $request,
+        ResponseInterface $response
+    ) {
+        $response->getBody()->write('Hello, World!');
+
+        return $response;
+    });
+
+    $app->get('/categorias', \App\Action\BajarDatos::class);
+    $app->get('/juegos', \App\Action\SacarJuegos::class);
+
+};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { JsonService } from './json.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pruebaAngular';
+
+  constructor(public json: JsonService){
+    this.json.getJson('http://localhost/pruebaProyecto/pruebaAngular/categorias').subscribe((res:any) => {
+      console.log(res);
+    });
+  }
 }

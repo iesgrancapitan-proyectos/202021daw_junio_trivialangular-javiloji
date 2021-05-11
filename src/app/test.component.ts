1a0522ae786426105bea2f74871b6ec997fd9f16
javiloji <javierlopera988@gmail.com>
2021-05-11 09:26:26 +0200

    Subiendo test

---
+++ src/app/test.component.ts
@@ -0,0 +1,14 @@
+import { MapType } from '@angular/compiler';
+import { Component } from '@angular/core';
+import { JsonService } from './json.service';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent {
+  
+ 
+}

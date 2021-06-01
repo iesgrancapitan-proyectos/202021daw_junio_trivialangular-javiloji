cea09d2ddafcc47a5385b769e669256fdc5a783f
Álvaro Leiva <alvaro.leiva@talenttools.es>
2021-06-01 22:29:54 +0200

    no se muestra pregunta y respuestas, y continuación de preguntas arreglado

--- src/app/preguntas/preguntas.component.ts
+++ src/app/preguntas/preguntas.component.ts
@@ -7,14 +7,14 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './preguntas.component.html',
   styleUrls: ['./preguntas.component.css']
 })
-export class PreguntasComponent implements OnInit, AfterViewInit  {
+export class PreguntasComponent implements OnInit, AfterViewInit {
 
-  public id:any = "";
-  public arrayCategorias:any = [];
-  public preguntas:any = [];
-  public respuestas:any = [];
+  public id: any = "";
+  public arrayCategorias: any = [];
+  public preguntas: any = [];
+  public respuestas: any = [];
 
-  public respuestaUnica:any = [];
+  public respuestaUnica: any = [];
 
 
   indexQuiz = 0;
@@ -22,76 +22,83 @@ export class PreguntasComponent implements OnInit, AfterViewInit  {
   respuestaValidaNombre = "";
   respuestaClicada = "";
   quizCompleted = false;
-  
-  constructor(private route: ActivatedRoute, private http:HttpClient) { 
 
-    this.http.get('http://localhost/proyectoDaw/familiaCategoria').toPromise().then(categorias => {
-
-      const jsonCategorias: any[] = Array.of(categorias);
-
-      for (let i = 0; i < jsonCategorias[0].length; i++) {
-        
-        if(jsonCategorias[0][i]['id_familia'] == this.route.snapshot.paramMap.get("id")){
-          this.arrayCategorias.push(jsonCategorias[0][i]['id_categoria']);
-        };
-      }
-    })
-
-    this.http.get('http://localhost/proyectoDaw/preguntas').toPromise().then(data => {
-
-      const usersJson: any[] = Array.of(data);
-
-      console.log(usersJson[0][2]['categoria']);
-
-      for (let j = 0; j < this.arrayCategorias.length; j++) {
-        // console.log(this.arrayCategorias[j]);
-
-        for (let i = 0; i < usersJson[0].length; i++) {
-          // console.log(this.arrayCategorias[j]);
-          if(usersJson[0][i]['categoria'] == this.arrayCategorias[j]){
-            
-            this.preguntas.push(usersJson[0][i]);
-          };
-          
-          
-        }
-      }
-
-      
-        console.log(this.preguntas.length);
-        console.log(this.preguntas);
-        this.shuffle(this.preguntas);
-        this.shuffle(this.respuestaUnica);
-      
-      // console.log(this.arrayCategorias[1]);
-
-    // console.log(this.preguntas);
-    // console.log(this.route.snapshot.paramMap.get("id"));
-    })    
-
-    this.http.get('http://localhost/proyectoDaw/respuestas').toPromise().then(data => {
-
-      const respuestasJson: any[] = Array.of(data);
-
-      console.log(respuestasJson);
-
-        for (let i = 0; i < respuestasJson[0].length; i++) {
-          this.respuestas.push(respuestasJson[0][i]);
-          
-        }
-
-      
-        console.log(this.respuestas);
-
-        this.avanzar();
-      
-      // console.log(this.arrayCategorias[1]);
-
-    // console.log(this.preguntas);
-    // console.log(this.route.snapshot.paramMap.get("id"));
-    })    
+  constructor(private route: ActivatedRoute, private http: HttpClient) {
 
     
+    
+
+    /* this.http.get('http://localhost/proyectoDaw/familiaCategoria').subscribe(categorias => {
+ 
+       const jsonCategorias: any[] = Array.of(categorias);
+ 
+       for (let i = 0; i < jsonCategorias[0].length; i++) {
+         
+         if(jsonCategorias[0][i]['id_familia'] == this.route.snapshot.paramMap.get("id")){
+           this.arrayCategorias.push(jsonCategorias[0][i]['id_categoria']);
+         };
+       }
+     })
+ 
+    // this.http.get('http://localhost/proyectoDaw/preguntas').toPromise().then(data => {
+     this.http.get('http://localhost/proyectoDaw/preguntas').subscribe(data => {
+       console.log("datos", data);
+       const usersJson: any[] = Array.of(data);
+       console.log(usersJson);
+       // console.log(usersJson[0][2]['categoria']);
+       console.log(this.arrayCategorias);
+       for (let j = 0; j < this.arrayCategorias.length; j++) {
+         // console.log(this.arrayCategorias[j]);
+         
+         
+         for (let i = 0; i < usersJson[0].length; i++) {
+           // console.log(this.arrayCategorias[j]);
+           if(usersJson[0][i]['categoria'] == this.arrayCategorias[j]){
+             
+             this.preguntas.push(usersJson[0][i]);
+           };
+           
+           
+         }
+       }
+ 
+       
+ 
+       
+         // console.log(this.preguntas.length);
+          console.log(this.preguntas);
+         this.shuffle(this.preguntas);
+         this.shuffle(this.respuestaUnica);
+       
+       // console.log(this.arrayCategorias[1]);
+ 
+     // console.log(this.preguntas);
+     // console.log(this.route.snapshot.paramMap.get("id"));
+     })    
+ 
+     this.http.get('http://localhost/proyectoDaw/respuestas').subscribe(data => {
+ 
+       const respuestasJson: any[] = Array.of(data);
+ 
+       // console.log(respuestasJson);
+ 
+         for (let i = 0; i < respuestasJson[0].length; i++) {
+           this.respuestas.push(respuestasJson[0][i]);
+           
+         }
+ 
+       
+         // console.log(this.respuestas);
+ 
+         this.avanzar();
+       
+       // console.log(this.arrayCategorias[1]);
+ 
+     // console.log(this.preguntas);
+     // console.log(this.route.snapshot.paramMap.get("id"));
+     })    
+ 
+     */
   }
 
   shuffle(array: any) {
@@ -106,54 +113,101 @@ export class PreguntasComponent implements OnInit, AfterViewInit  {
     return array;
   }
 
+  calcularSiguientePregunta(){
+      for (let i = 0; i < this.respuestas.length; i++) {
+        if (this.respuestas[i]["idPregunta"] == this.preguntas[this.indexQuiz]["id"]) {
+          this.respuestaUnica.push(this.respuestas[i]);
+        }
+      }
+
+  }
+
 
   avanzar() {
     this.respuestaValidaNombre = "";
     this.respuestaClicada = "";
-    // console.log(this.indexQuiz, this.arrayBanderas.length)
     if (this.preguntas.length - 1 > this.indexQuiz) {
       this.indexQuiz++;
     } else {
-      // console.log(this.aciertos);
+     
       this.quizCompleted = true;
     }
+    this.respuestaUnica = [];
+    this.calcularSiguientePregunta();
 
-    // console.log(this.preguntas[this.indexQuiz]["id"]);
-
-    // console.log(this.respuestas[0]);
-    console.log("hola");
-
-    console.log(this.preguntas[this.indexQuiz]["id"]);
-
-    for (let i = 0; i < this.respuestas.length; i++) {
-      if(this.respuestas[i]["idPregunta"]==this.preguntas[this.indexQuiz]["id"]){
-        console.log(this.respuestas[i]["idPregunta"]);
-
-        this.respuestaUnica.push(this.respuestas[i]);
-
-      }
-    }
-
-    // console.log(this.respuestas[0]);
-    console.log(this.respuestaUnica);
   }
 
   elegirRespuesta(respuesta: any, arrayRespuestas: any) {
-    // console.log(respuesta, arrayRespuestas);
+    
     let that = this;
     this.respuestaClicada = respuesta.respuesta;
-    // console.log(respuesta.respuesta);
-    if (respuesta.valida) {
+    
+    if (parseInt(respuesta.valida)) {
       this.aciertos++;
     }
-    console.log(respuesta);
-
-    this.respuestaUnica.forEach((element: any) => {
+    arrayRespuestas.forEach((element: any) => {
       if (element.valida == 1) {
         that.respuestaValidaNombre = element.respuesta;
-        console.log(that.respuestaValidaNombre);
+        
       }
     });
+
+  }
+  getRespuestas() {
+    
+
+    let that = this;
+    this.http.get('http://localhost/proyectoDaw/respuestas').subscribe((data : any) => {
+        that.respuestas = data;
+          that.calcularSiguientePregunta();
+          that.shuffle(this.respuestaUnica);
+
+    })
+  }
+  getPreguntas() {
+    let that = this;
+    this.http.get('http://localhost/proyectoDaw/preguntas').subscribe( ( data : any) => {
+      /*for (j = 0; j < this.arrayCategorias.length; j++) {
+        for (i = 0; i < data.length; i++) {
+          if (data[i]['categoria'] == this.arrayCategorias[j]) {
+            console.log(data[i]['categoria']);
+            that.preguntas.push(data[i]);
+          }
+
+        }
+
+      }*/
+      
+      for (let i = 0; i < data.length; i++) {
+        if(that.arrayCategorias.includes(data[i].categoria)){
+          that.preguntas.push(data[i]);
+        }
+      }
+
+      
+      that.shuffle(this.preguntas);
+      
+
+      that.getRespuestas();
+
+   
+    })
+  }
+  getCategorias() {
+    let that = this;
+    this.http.get('http://localhost/proyectoDaw/familiaCategoria').subscribe((categorias : any) => {
+      
+      for (let i = 0; i < categorias.length; i++) {
+        if (categorias[i]['id_familia'] == this.route.snapshot.paramMap.get("id")) {
+          that.arrayCategorias.push(categorias[i]['id_categoria']);
+        };
+      }
+
+      that.getPreguntas();
+      
+    })
+
+   
 
   }
 
@@ -161,6 +215,9 @@ export class PreguntasComponent implements OnInit, AfterViewInit  {
   }
 
   ngOnInit(): void {
+    this.getCategorias();
+    
+    
   }
 
 }
